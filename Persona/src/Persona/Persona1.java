dec8641bd47811bc35954d537eafded9c281930a
AlexBonilla00 <abm0056@alu.medac.es>
2024-03-14 16:07:17 +0100

    Proyecto personas creado

---
+++ Persona/src/Persona/Persona1.java
@@ -0,0 +1,115 @@
+package Persona;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona1 {
+
+    /**
+     * Default constructor
+     */
+    public Persona1() {
+    }
+
+    /**
+     * 
+     */
+    private String DNI ;
+
+    /**
+     * 
+     */
+    private String nombre ;
+
+    /**
+     * 
+     */
+    private String apellidos ;
+
+    /**
+     * 
+     */
+    private int edad ;
+
+    /**
+     * Crea una persona a partir de su dni, nombre, apellidos y edad.
+     * @param DNI 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public void Persona(String DNI, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * establece el dni
+     * @param DNI
+     */
+    public void setDNI(String DNI) {
+        // TODO implement here
+    }
+
+    /**
+     * obtiene el dni
+     * @return
+     */
+    public String getDNI() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Establece el nombre
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * Obtiene el nombre
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Establece apellido
+     * @return
+     */
+    public String setApellido() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Obtiene apellido
+     * @param apellido:String
+     */
+    public void getApellido(String apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * establece edad
+     * @return
+     */
+    public int setEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * obtiene edad
+     * @param edad
+     */
+    public void getEdad(int edad) {
+        // TODO implement here
+    }
+
+}
