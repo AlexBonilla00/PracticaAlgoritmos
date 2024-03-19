8940ae78a8f07ab07ee5776271082bfff32b1cad
AlexBonilla00 <abm0056@alu.medac.es>
2024-03-19 17:39:51 +0100

    Figuras java

--- Figuras/src/app/Main_Triangulo.java
+++ Figuras/src/app/Main_Triangulo.java
@@ -3,7 +3,7 @@ import modelo.Triangulo;
 public class Main_Triangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Triangulo t1 = new  Triangulo(3.14,5.12);
 		Triangulo t2 = new  Triangulo(2.55,4.99);
 	    double area1 = t1.calcularArea();
