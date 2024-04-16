fa7c1ca34186d22e6a1767b97e20ff27364d88da
AlexBonilla00 <abm0056@alu.medac.es>
2024-04-16 17:27:38 +0200

    Práctica Algoritmos Entornos de Desarrollo

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -1,35 +1,7 @@
 package Util;
 
 public abstract class  Algoritmos {
-	/**
-     * Calcula el numero de fibonacci de un numero.
-     *
-     * @param numero el numero para calcular su fibonacci.
-     * @return el número en fibonacci.
-     */
-    public static int fibonacci(int numero) {
-        if (numero <= 1) {
-            return numero;
-        }
-        else {
-        	return fibonacci(numero - 1) + fibonacci(numero - 2); 
-        }
-       
-    }
 
-	/**
-     * Calcula el factorial de un numero.
-     *
-     * @param n El numero del cual se calculará el factorial.
-     * @return El factorial del numero n.
-     */
-    public static int factorial(int n) {
-        int factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        return factorial;
-    }
 
 }
 	
