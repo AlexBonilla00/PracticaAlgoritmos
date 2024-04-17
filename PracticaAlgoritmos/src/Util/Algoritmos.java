9cb9e0356f3eeadf6ba012f9905374ced3786274
AlexBonilla00 <abm0056@alu.medac.es>
2024-04-17 17:27:41 +0200

    Práctica Algoritmos Entornos de Desarrollo

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -16,4 +16,18 @@ public class Algoritmos {
         }
        
     }
+    
+    /**
+     * Calcula el factorial de un numero.
+     *
+     * @param n El numero del cual se calculará el factorial.
+     * @return El factorial del numero n.
+     */
+    public static int factorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
